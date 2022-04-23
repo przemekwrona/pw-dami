644971587df330908a4324c57a6971f738e10139
pwrona <przemekwrona25@gmail.com>
2022-04-24 00:19:12 +0200

    implement knn algorithm

--- python/main.py
+++ python/main.py
@@ -1,5 +1,25 @@
+import pandas as pd
+import numpy as np
+
 from dami.files import loader
+from dami import knn
 
 if __name__ == '__main__':
     print("Init project")
     loader.load_txt()
+
+    data = np.array([
+        [150, 50],
+        [160, 55],
+        [183, 80],
+        [200, 99],
+        [140, 45],
+        [150, 50],
+        [186, 74],
+        [183, 84],
+        [150, 50],
+        [178, 77],
+        [154, 62]
+    ])
+
+    knn.knn(data, 2)

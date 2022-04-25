4fa0e9e14c80ce44e5f75138c990ed7e323d2240
przemekwrona <50826441+przemekwrona@users.noreply.github.com>
2022-04-25 22:34:31 +0200

    plot knn results (#4)

---
+++ python/dami/plots/plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter_clazz(data):
+    for clazz in data:
+        vectors_in_class = np.asarray(clazz)
+        x = vectors_in_class[:, 0]
+        y = vectors_in_class[:, 1]
+        plt.scatter(x, y)
+
+
+def draw_classified_2d_data(data):
+    scatter_clazz(data)
+
+    plt.xlabel("Height")
+    plt.ylabel("Weight")
+    plt.grid()
+    plt.show()
+
+
+def draw_classified_2d_data_with_test(data, x, y, r=5):
+    scatter_clazz(data)
+
+    plt.scatter(x, y, color='grey')
+    circle = plt.Circle((x, y), r, color='grey', fill=False)
+    plt.gca().add_patch(circle)
+
+    plt.xlabel("Height")
+    plt.ylabel("Weight")
+    plt.grid()
+    plt.show()

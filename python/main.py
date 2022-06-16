0a4ee96c93432bb1a2e56bf06b424aa91979d303
pwrona <przemekwrona25@gmail.com>
2022-06-16 18:16:06 +0200

    feat: add k-fold validation

--- python/main.py
+++ python/main.py
@@ -5,26 +5,26 @@ from python.dami.experiments import lymphography
 from python.dami.experiments import spambase
 
 if __name__ == '__main__':
-    # gender.run_experiment(k=1, fold=3, mode='STAND')
-    # gender.run_experiment(k=3, fold=3, mode='STAND')
-    # gender.run_experiment(k='optimal', fold=3, mode='STAND')
-    # gender.run_experiment(k='max', fold=3, mode='STAND')
+    gender.run_experiment(k=1, fold=3, mode='STAND')
+    gender.run_experiment(k=3, fold=3, mode='STAND')
+    gender.run_experiment(k='optimal', fold=3, mode='STAND')
+    gender.run_experiment(k='max', fold=3, mode='STAND')
 
-    heart.run_experiment(k=1, fold=3, mode='STAND')
-    heart.run_experiment(k=3, fold=3, mode='STAND')
-    heart.run_experiment(k='optimal', fold=3, mode='STAND')
-    heart.run_experiment(k='max', fold=3, mode='STAND')
+    # heart.run_experiment(k=1, fold=3, mode='STAND')
+    # heart.run_experiment(k=3, fold=3, mode='STAND')
+    # heart.run_experiment(k='optimal', fold=3, mode='STAND')
+    # heart.run_experiment(k='max', fold=3, mode='STAND')
 
-    heart.run_experiment(k=1, fold=3, mode='NORM')
-    heart.run_experiment(k=3, fold=3, mode='NORM')
-    heart.run_experiment(k='optimal', fold=3, mode='NORM')
-    heart.run_experiment(k='max', fold=3, mode='NORM')
+    # heart.run_experiment(k=1, fold=3, mode='NORM')
+    # heart.run_experiment(k=3, fold=3, mode='NORM')
+    # heart.run_experiment(k='optimal', fold=3, mode='NORM')
+    # heart.run_experiment(k='max', fold=3, mode='NORM')
 
     # lymphography.run_experiment(k=1, fold=3, mode='STAND')
     # lymphography.run_experiment(k=3, fold=3, mode='STAND')
     # lymphography.run_experiment(k='optimal', fold=3, mode='STAND')
     # lymphography.run_experiment(k='max', fold=3, mode='STAND')
-    #
+
     # lymphography.run_experiment(k=1, fold=3, mode='NORM')
     # lymphography.run_experiment(k=3, fold=3, mode='NORM')
     # lymphography.run_experiment(k='optimal', fold=3, mode='NORM')
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     # spambase.run_experiment(k=3, fold=3, mode='STAND')
     # spambase.run_experiment(k='optimal', fold=3, mode='STAND')
     # spambase.run_experiment(k='max', fold=3, mode='STAND')
-    #
+
     # spambase.run_experiment(k=1, fold=3, mode='NORM')
     # spambase.run_experiment(k=3, fold=3, mode='NORM')
     # spambase.run_experiment(k='optimal', fold=3, mode='NORM')
